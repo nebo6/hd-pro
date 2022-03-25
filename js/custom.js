9d909e781e5a1adba70ee265e489ba89e6f59f00
Olzhas <olzh.zhussipbek@yandex.kz>
2022-03-26 01:57:11 +0600

    page title. dropdowns added

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,23 @@ function initAccordion() {
     })
 }
 
+function initDropdown() {
+    $(document).on("click", function(e) {
+        const target = $(e.target);
+        if (!(target.closest("[data-dropdown-parent]").length)) {
+            $(".dropdown_active").removeClass("dropdown_active")
+        }
+    })
+    
+    $(document).on("click", "[data-dropdown-target]", function() {
+        const target = $(this).data("dropdown-target");
+        const dropdown = $("[data-dropdown=" + target + "]")
+        dropdown.toggleClass("dropdown_active");
+        $("[data-dropdown]").not(dropdown).removeClass("dropdown_active")
+    })
+}
+
 $(function() {
     initAccordion();
+    initDropdown();
 })
