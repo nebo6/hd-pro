1a025c55b00e825be2221958f3fd089090772eb1
Olzhas <olzh.zhussipbek@yandex.kz>
2022-03-31 01:52:40 +0600

    unstyled modal and forms added

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+// init simple accordio
 function initAccordion() {
     const accordion = $(".js-accordion");
 
@@ -7,7 +8,7 @@ function initAccordion() {
         body.stop().slideToggle(250)
     })
 }
-
+// init simple dropdown
 function initDropdown() {
     $(document).on("click", function(e) {
         const target = $(e.target);
@@ -23,8 +24,57 @@ function initDropdown() {
         $("[data-dropdown]").not(dropdown).removeClass("dropdown_active")
     })
 }
+// on input type="file" changed callback
+function onFileSelected(event) {
+    const selectedFile = event.target.files[0];
+    const reader = new FileReader();
+  
+    const image = $(this).siblings(".img");
+    // imgtag.title = selectedFile.name;
+  
+    reader.onload = function(event) {
+        image.css("background-image", "url('"+event.target.result+"')");
+        console.log(event.target.result);
+    };
+  
+    if (selectedFile)
+        reader.readAsDataURL(selectedFile);
+    image.css("background-image", "");
+}
+// on clients remove icon clicked callback
+function onClientsRemvoeConfirmed() {
+    const id = parseInt($(this).data("id"))
+    if (isNaN(id)) alert("Something went wrong")
+    $("body").addClass("loading");
+    setTimeout(function() {
+        const target = $('[data-clients-row="'+id+'"]')
+        target.remove()
+        $("body").removeClass("loading");
+        console.log("removed", id);
+    }, 2000)
+}
+
+function onClientsRemoved(id) {
+    const modal = $('[data-mymodal-id="remove-modal"]'); // get delete nitification modal
+    $(document).on("opening", modal, function() {
+        if (!id) $(this).find(".id").text("NONE")
+        $(this).find(".id").text(id);
+        $(this).data("id", id);
+    });
+    $(document).on("confirmation", modal, onClientsRemvoeConfirmed);
+    $(document).on("closed", modal, function() {
+        $(this).find(".id").text("NONE");
+        $(this).data("id", undefined);
+        $(document).off("confirmation", modal, onClientsRemvoeConfirmed);
+    });
+
+    modal.mymodal().open();
+}
 
 $(function() {
     initAccordion();
     initDropdown();
+
+    $('.js-form-file').on("change", onFileSelected)
+    
 })
