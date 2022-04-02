587f2ddb91e99675c4dd24a8e4570fc50ad4a056
Olzhas <olzh.zhussipbek@yandex.kz>
2022-04-02 17:03:37 +0600

    some fixes

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+function getLanguage() {
+    return document.documentElement.lang
+}
 // init simple accordio
 function initAccordion() {
     const accordion = $(".js-accordion");
@@ -42,29 +45,25 @@ function onFileSelected(event) {
     image.css("background-image", "");
 }
 // on clients remove icon clicked callback
-function onClientsRemvoeConfirmed() {
-    const id = parseInt($(this).data("id"))
-    if (isNaN(id)) alert("Something went wrong")
+function onClientsRemvoeConfirmed(id) {
+    if (isNaN(parseInt(id))) alert("Something went wrong")
     $("body").addClass("loading");
     setTimeout(function() {
         const target = $('[data-clients-row="'+id+'"]')
         target.remove()
         $("body").removeClass("loading");
-        console.log("removed", id);
     }, 2000)
 }
 
 function onClientsRemoved(id) {
-    const modal = $('[data-mymodal-id="remove-modal"]'); // get delete nitification modal
+    const modal = $('.mymodal_confirmation'); // get delete confiramation modal
     $(document).on("opening", modal, function() {
-        if (!id) $(this).find(".id").text("NONE")
-        $(this).find(".id").text(id);
-        $(this).data("id", id);
+        const title = getLanguage() === "ru" ? `Вы уверены что хотите удалить ID ${id}` : `Are you sure you want to delete ID${id}`
+        $(this).find(".confiramtion-title").text(title);
     });
-    $(document).on("confirmation", modal, onClientsRemvoeConfirmed);
+    $(document).on("confirmation", modal, onClientsRemvoeConfirmed.bind(modal, id));
     $(document).on("closed", modal, function() {
-        $(this).find(".id").text("NONE");
-        $(this).data("id", undefined);
+        $(this).find(".confiramtion-title").text("")
         $(document).off("confirmation", modal, onClientsRemvoeConfirmed);
     });
 
@@ -73,6 +72,57 @@ function onClientsRemoved(id) {
 
 function onEstimatesShowed(id) {
     alert("show " + id);
+}
+
+function getRadioPrev(event) {
+    const _target = event.target
+    const list = $(_target).closest(".radio__list")
+    const prevInput = list.find(".radio__prev")
+    const prevValue = prevInput.val()
+
+    return {
+        input: prevInput,
+        value: prevValue,
+        radio: list.find('[value="'+prevValue+'"]')
+    }
+}
+
+function onEstimatesChanged(event, id) {
+    const prev = getRadioPrev(event)
+    console.log("#", id, "estimated status changed to", event.target.value);
+    prev.input.val(event.target.value)
+    /* 
+        if reauqest will fail
+        then change value again to prev
+        prev.radio.prop("checked", true)
+        else change prevInput value to checked
+        prev.input.val(event.target.value)
+    */
+}
+
+function onCarsStatusChanged(event, id) {
+    const prev = getRadioPrev(event)
+    console.log("#", id, "cars status changed to", event.target.value);
+    prev.input.val(event.target.value)
+    /* 
+        if reauqest will fall
+        then change value again to prev
+        prev.radio.prop("checked", true)
+        else change prevInput value to checked
+        prev.input.val(event.target.value)
+    */
+}
+
+function onInvoicesEdited(id) {
+    console.log("edit invoice", id);
+}
+
+function onContactsActiveChanged(event, id) {
+    console.log("contacts active changed", id, event.target.checked);
+}
+
+function onContactsPaymentChanged(event, id) {
+    console.log("contacts payment status changed", id, event.target.checked);
 }
 
 $(function() {
